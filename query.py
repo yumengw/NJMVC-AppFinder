f6aabe4ec40e8ab53ca9bb83840a01ad57edda7a
Yumeng Wang <yumengwang@Yumengs-MacBook-Pro.local>
2021-04-04 08:19:37 -0400

    fix bug in time selection

--- query.py
+++ query.py
@@ -6,9 +6,10 @@ from bs4 import BeautifulSoup
 
 locfile = 'locations.csv'
 URL_prefix = 'https://telegov.njportal.com/njmvc/AppointmentWizard/11'
-
+db_host = 'https://telegov.njportal.com/'
 non_avail = 'Currently, there are no appointments available at this location.'
 for line in csv.DictReader(open(locfile)):
+
     if int(line['time from home']) > 60: continue
     page = requests.get(URL_prefix + '/' + line['url'])
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -17,24 +18,21 @@ for line in csv.DictReader(open(locfile)):
         print(line['Name'] + ' no')
         continue
     else:
-        ## find date
-        datee = soup.findAll('label', attrs={'class':'control-label'})[-1].text
-        date_string = ' '.join(datee.split()[-3:])
+        ## find available slots
+        timee_list = soup.findAll('a', attrs={'class':'text-primary', 'href':True})
+        timee = timee_list[-1]['href'] ## get latest time
 
-        timee_list = soup.findAll('a', attrs={'style':'text-decoration:none !important;'})
-        if len(timee_list) < 1: continue
-        timee = timee_list[-1].text ## get latest time
-        time_string = ' '.join(timee.split()[:-1])
-
-        date_obj = datetime.strptime(date_string + time_string, "%B %d, %Y:%I:%M %p")
+        date_string = timee.split('/')[-2]
+        time_string = timee.split('/')[-1]
+        date_obj = datetime.strptime(date_string + ' ' + time_string, "%Y-%m-%d %H%M")
 
         if date_obj < datetime(2021, 5, 2): 
             print(line)
-            webbrowser.open(URL_prefix + '/' + line['url'] + '/' + str(date_obj.strftime("%Y-%m-%d/%H%M")))
+            webbrowser.open(db_host + timee)
         else:
             print(line['Name'] + ' earlist ' + str(date_obj.date()))
         #results = soup.find(id='locationsDiv')
         #print(results.prettify())
         #with open("output.html", "w") as file:
         #    file.write(str(soup))
-        #break
+        break
--- query_dev.py
+++ query_dev.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 locfile = 'locations.csv'
 URL_prefix = 'https://telegov.njportal.com/njmvc/AppointmentWizard/11'
-
+db_host = 'https://telegov.njportal.com/'
 non_avail = 'Currently, there are no appointments available at this location.'
 for line in csv.DictReader(open(locfile)):
 
@@ -20,18 +20,19 @@ for line in csv.DictReader(open(locfile)):
     else:
         ## find available slots
         timee_list = soup.findAll('a', attrs={'class':'text-primary', 'href':True})
-        timee = timee_list[-1].text ## get latest time
-        time_string = ' '.join(timee.split()[:-1])
+        timee = timee_list[-1]['href'] ## get latest time
 
-        date_obj = datetime.strptime(date_string + time_string, "%B %d, %Y:%I:%M %p")
+        date_string = timee.split('/')[-2]
+        time_string = timee.split('/')[-1]
+        date_obj = datetime.strptime(date_string + ' ' + time_string, "%Y-%m-%d %H%M")
 
         if date_obj < datetime(2021, 5, 2): 
             print(line)
-            webbrowser.open(URL_prefix + '/' + line['url'] + '/' + str(date_obj.strftime("%Y-%m-%d/%H%M")))
+            webbrowser.open(db_host + timee)
         else:
             print(line['Name'] + ' earlist ' + str(date_obj.date()))
         #results = soup.find(id='locationsDiv')
         #print(results.prettify())
         #with open("output.html", "w") as file:
         #    file.write(str(soup))
-        #break
+        break
